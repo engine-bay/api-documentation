147850262bed9ad9b762916670bdc1d4ec128e24
Janus Liebenberg <janus.liebenberg@entelect.co.uk>
2023-11-23 18:21:34 +0000

    #major. update to dotnet8

---
+++ EngineBay.ApiDocumentation.Tests/ApiDocumentationTests.cs
@@ -0,0 +1,13 @@
+namespace EngineBay.ApiDocumentation.Tests
+{
+    using Xunit;
+
+    public class ApiDocumentationTests
+    {
+        [Fact]
+        public void ControlTest()
+        {
+            Assert.True(true);
+        }
+    }
+}
